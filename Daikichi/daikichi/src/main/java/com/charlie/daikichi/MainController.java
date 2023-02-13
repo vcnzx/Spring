0644e0aa984f8884de9b4b95105225205b7cf5c2
vcntu <vcngit@gmail.com>
2023-02-12 21:55:06 -0800

    added Daikichi Path Variables

--- Daikichi/daikichi/src/main/java/com/charlie/daikichi/MainController.java
+++ Daikichi/daikichi/src/main/java/com/charlie/daikichi/MainController.java
@@ -1,5 +1,6 @@
 package com.charlie.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,24 @@ public class MainController {
     @RequestMapping("/tomorrow")
     public String tomorrow(){
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+
+    @RequestMapping("/travel/{city}")
+    public String travel(@PathVariable("city") String city){
+        return "Congratulation! You will soon travel to " + city;
+    }
+
+    @RequestMapping("/lotto/{number}")
+    public String lotto(@PathVariable("number") Integer number){
+        if(number%2 == 0){
+            return "You will take a grand journey in the near future, but be weary of tempting offers.";
+        }
+        else if (number%2 == 1){
+            return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+        }
+        else {
+            return "Please enter a number";
+        }
     }
     
 }
